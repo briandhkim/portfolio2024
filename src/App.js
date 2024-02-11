7dd19f0fa0940ced9850dfbf795ee89974d49c1a
Brian Kim <briandhkim93@gmail.com>
2024-02-10 22:08:03 -0800

    landing page setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import Landing from './component/Landing';
 
 function App() {
+    const [unlocked, setUnlocked] = useState(false);
+
     return (
         <div className="App">
-            <Landing />
+            <Landing unlocked={unlocked} setUnlocked={setUnlocked} />
         </div>
     );
-};
+}
 
 export default App;
--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -1,11 +1,28 @@
-const Landing = () => {
+import { LockClosedIcon, LockOpenIcon } from '@heroicons/react/24/outline';
+
+const Landing = ({ unlocked, setUnlocked }) => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content ">
-                <div className="max-w-md">
-                    <div className="mockup-code">
-                        <pre data-prefix="$"><code>hello there</code></pre> 
-                        <pre data-prefix=">" className="text-success"><code></code></pre>
+        <div className=" min-h-screen bg-base-200 flex justify-center items-center">
+            <div className=" ">
+                <div className=" ">
+                    <div className="mockup-code w-80">
+                        <pre className="text-success">
+                            <code>brian@bdhk</code>
+                        </pre>
+                        <pre data-prefix="$" className="">
+                            <code>hi</code>
+                        </pre>
+                    </div>
+                    <div className="text-center mt-4">
+                        <button
+                            className={`text-primary swap swap-rotate ${!unlocked && 'swap-active'}`}
+                            onClick={() => setUnlocked(!unlocked)}
+                        >
+                            <LockClosedIcon
+                                className={`h-10 w-10 swap-on ${!unlocked && 'animate-pulse'}`}
+                            />
+                            <LockOpenIcon className="h-10 w-10 swap-off" />
+                        </button>
                     </div>
                 </div>
             </div>
@@ -13,4 +30,4 @@ const Landing = () => {
     );
 };
 
-export default Landing
+export default Landing;
