d7696cad9fa09b728aa8cc60e212ad9430eedcef
Brian Kim <briandhkim93@gmail.com>
2024-02-15 02:12:01 -0800

    working on themes

--- src/component/NavbarTw.jsx
+++ src/component/NavbarTw.jsx
@@ -1,6 +1,8 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 
 export default function NavbarTw({ page }) {
     const navigation = [
@@ -9,7 +11,21 @@ export default function NavbarTw({ page }) {
         { name: 'Skills', href: '#', current: false },
         { name: 'Contact', href: '#', current: false },
     ];
+    const themes = [
+        'light',
+        'dark',
+        'retro',
+        'cyberpunk',
+        'lofi',
+        'wireframe',
+        'business',
+        'night',
+        'dim',
+        'nord',
+        'sunset',
+    ];
     const [currentPage, setCurrentPage] = useState('About');
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     function classNames(...classes) {
         return classes.filter(Boolean).join(' ');
@@ -26,7 +42,7 @@ export default function NavbarTw({ page }) {
     return (
         <Disclosure
             as="nav"
-            className="bg-transparent shadow top-0 z-10 sticky w-full float-right clear-both"
+            className="bg-base-200 bg-opacity-80 shadow top-0 z-10 sticky w-full float-right clear-both "
         >
             {({ open }) => (
                 <>
@@ -66,12 +82,11 @@ export default function NavbarTw({ page }) {
                                         <button
                                             key={item.name}
                                             // href={item.href}
-                                            className={classNames(
+                                            className={`${classNames(
                                                 item.name === currentPage
-                                                    ? 'border-indigo-500 text-gray-900'
-                                                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                                                'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'
-                                            )}
+                                                    ? 'border-primary text-accent'
+                                                    : 'border-transparent hover:border-primary hover:text-accent text-primary'
+                                            )} ${isDarkMode && item.name !== currentPage ? '' : ''} inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium`}
                                             onClick={() =>
                                                 navHandler(item.name)
                                             }
@@ -85,38 +100,12 @@ export default function NavbarTw({ page }) {
                                 <label className="cursor-pointer grid place-items-center">
                                     <input
                                         type="checkbox"
-                                        value="dim"
-                                        className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+                                        value="business"
+                                        className="toggle theme-controller row-start-1 col-start-1 col-span-2 bg-amber-300 border-blue-300 [--tglbg:theme(colors.blue.400)] checked:bg-blue-300 checked:border-blue-800 checked:[--tglbg:theme(colors.blue.900)]"
+                                        onChange={e =>
+                                            setIsDarkMode(e.target.checked)
+                                        }
                                     />
-                                    <svg
-                                        className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="14"
-                                        height="14"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <circle cx="12" cy="12" r="5" />
-                                        <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-                                    </svg>
-                                    <svg
-                                        className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="14"
-                                        height="14"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                                    </svg>
                                 </label>
 
                                 {/* Profile dropdown */}
@@ -191,6 +180,41 @@ export default function NavbarTw({ page }) {
                                         </Menu.Items>
                                     </Transition>
                                 </Menu>
+                                <div className="dropdown ">
+                                    <div
+                                        tabIndex={0}
+                                        role="button"
+                                        className="btn m-1"
+                                    >
+                                        Theme
+                                        <svg
+                                            width="12px"
+                                            height="12px"
+                                            className="h-2 w-2 fill-current opacity-60 inline-block"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 2048 2048"
+                                        >
+                                            <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+                                        </svg>
+                                    </div>
+                                    <ul
+                                        tabIndex={0}
+                                        className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
+                                    >
+                                        {themes.map((theme, idx) => (
+                                            <li key={idx}>
+                                                <input
+                                                    type="radio"
+                                                    name="theme-dropdown"
+                                                    className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                                    aria-label={theme}
+                                                    value={theme}
+                                                />
+                                            </li>
+                                        ))}
+
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
