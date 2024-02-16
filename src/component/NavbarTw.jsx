a7801e278775debd2e1aa5f5eb38fb55c91ee701
Brian Kim <briandhkim93@gmail.com>
2024-02-16 01:15:34 -0800

    finishing up theme controller

--- src/component/NavbarTw.jsx
+++ src/component/NavbarTw.jsx
@@ -1,8 +1,9 @@
 import { Fragment, useEffect, useRef, useState } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Disclosure, Menu, RadioGroup, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
+import { faPalette } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavbarTw({ page }) {
     const navigation = [
@@ -26,6 +27,9 @@ export default function NavbarTw({ page }) {
     ];
     const [currentPage, setCurrentPage] = useState('About');
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const [currentTheme, setCurrentTheme] = useState('retro');
+
+    const themeDropdown = useRef(null);
 
     function classNames(...classes) {
         return classes.filter(Boolean).join(' ');
@@ -34,6 +38,13 @@ export default function NavbarTw({ page }) {
         document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
         setCurrentPage(id);
     }
+
+    useEffect(() => {
+        window.addEventListener('click', e => {
+            if (!themeDropdown.current.contains(e.target))
+                themeDropdown.current.open = false;
+        });
+    }, []);
 
     useEffect(() => {
         setCurrentPage(page);
@@ -97,7 +108,7 @@ export default function NavbarTw({ page }) {
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <label className="cursor-pointer grid place-items-center">
+                                {/* <label className="cursor-pointer grid place-items-center">
                                     <input
                                         type="checkbox"
                                         value="business"
@@ -106,100 +117,24 @@ export default function NavbarTw({ page }) {
                                             setIsDarkMode(e.target.checked)
                                         }
                                     />
-                                </label>
-
-                                {/* Profile dropdown */}
-                                <Menu as="div" className="relative ml-3">
-                                    <div>
-                                        <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                                            <span className="sr-only">
-                                                Open user menu
-                                            </span>
-                                            <img
-                                                className="h-8 w-8 rounded-full"
-                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                alt=""
-                                            />
-                                        </Menu.Button>
-                                    </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-200"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
-                                    >
-                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Sign out
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
-                                <div className="dropdown ">
-                                    <div
+                                </label> */}
+                                <details
+                                    className="dropdown"
+                                    ref={themeDropdown}
+                                >
+                                    <summary
                                         tabIndex={0}
                                         role="button"
                                         className="btn m-1"
                                     >
-                                        Theme
-                                        <svg
-                                            width="12px"
-                                            height="12px"
-                                            className="h-2 w-2 fill-current opacity-60 inline-block"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 2048 2048"
-                                        >
-                                            <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-                                        </svg>
-                                    </div>
+                                        <FontAwesomeIcon
+                                            icon={faPalette}
+                                            className="fa-2xl"
+                                        />
+                                    </summary>
                                     <ul
                                         tabIndex={0}
-                                        className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
+                                        className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52 absolute right-0 z-10 menu"
                                     >
                                         {themes.map((theme, idx) => (
                                             <li key={idx}>
@@ -212,9 +147,8 @@ export default function NavbarTw({ page }) {
                                                 />
                                             </li>
                                         ))}
-
                                     </ul>
-                                </div>
+                                </details>
                             </div>
                         </div>
                     </div>
