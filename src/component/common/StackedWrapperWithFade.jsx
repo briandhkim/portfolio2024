e0be75d0632a0468d9631afab007aea946c79830
Brian Kim <briandhkim93@gmail.com>
2024-02-20 22:56:22 -0800

    Clean up wrapper component

---
+++ src/component/common/StackedWrapperWithFade.jsx
@@ -0,0 +1,55 @@
+import { Transition } from '@headlessui/react';
+import { forwardRef } from 'react';
+
+const StackedWrapperWithFade = forwardRef(
+    function StackedWrapperWithFade(props, ref) {
+        const {
+            fadeIn = false,
+            unmount = false,
+            children,
+            className = '',
+            sectionTitle = '',
+            id = '',
+            headerBg = 'bg-base-200',
+            bodyBg = 'bg-base-100',
+        } = props;
+
+        return (
+            <section
+                className={`${bodyBg} ${className} min-h-screen`}
+                id={id}
+                ref={ref}
+            >
+                <Transition
+                    show={fadeIn}
+                    enter="transition-opacity ease-in-out duration-[1200ms]"
+                    enterFrom="opacity-20"
+                    enterTo="opacity-100"
+                    leave="transition-opacity ease-in-out duration-[1200ms]"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    unmount={unmount}
+                >
+                    <div className={`${headerBg} pb-32 pt-16`}>
+                        <header className="py-10">
+                            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                                <h1 className="text-3xl font-bold tracking-tight ">
+                                    {sectionTitle}
+                                </h1>
+                            </div>
+                        </header>
+                    </div>
+
+                    <div className="-mt-32">
+                        <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+                            {/* Your content */}
+                            {children}
+                        </div>
+                    </div>
+                </Transition>
+            </section>
+        );
+    }
+);
+
+export default StackedWrapperWithFade;
--- src/component/pages/Skills.jsx
+++ src/component/pages/Skills.jsx
@@ -1,9 +1,9 @@
 import { Fragment, useEffect, useRef, useState } from 'react';
-import { RadioGroup, Transition } from '@headlessui/react';
+import { RadioGroup } from '@headlessui/react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useOnScreen } from '../../hooks/useOnScreen';
-import StackedWrapper from '../common/StackedWrapper';
 import { skillList } from '../../util/constants';
+import StackedWrapperWithFade from '../common/StackedWrapperWithFade';
 
 const skillFilters = [
     { name: 'All' },
@@ -55,119 +55,109 @@ const Skills = ({ setCurrentPage }) => {
     }, [skillFilter]);
 
     return (
-        <StackedWrapper
+        <StackedWrapperWithFade
             sectionTitle="Skills"
             id="Skills"
             headerBg="bg-base-100"
             // bodyBg="bg-gradient-to-b from-base-100 from-10% via-base-100 via-45% to-base-300 to-70%"
             bodyBg="bg-base-300"
             ref={sectionRef}
+            fadeIn={fadeIn}
         >
-            <Transition
-                show={fadeIn}
-                enter="transition-opacity ease-in-out duration-[1200ms]"
-                enterFrom="opacity-20"
-                enterTo="opacity-100"
-                leave="transition-opacity ease-in-out duration-[1200ms]"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                unmount={false}
-            >
-                <div className="bg-base-200 px-6 py-8 rounded-2xl ">
-                    <h2 className="text-lg font-medium text-gray-500">
-                        Some of the languages, frameworks, and tools that I had
-                        opportunities to work with
-                    </h2>
+            <div className="bg-base-200 px-6 py-8 rounded-2xl ">
+                <h2 className="text-lg font-medium text-gray-500">
+                    Some of the languages, frameworks, and tools that I had
+                    opportunities to work with
+                </h2>
 
-                    <RadioGroup
-                        value={skillFilter}
-                        onChange={setSkillFilter}
-                        className="mt-2"
-                    >
-                        <RadioGroup.Label className="sr-only">
-                            Skill filter
-                        </RadioGroup.Label>
-                        <div className="flex">
-                            <div className="flex items-center font-normal md:font-medium mr-1 ">
-                                Filter:
-                            </div>
-                            {skillFilters.map((option, idx) => (
-                                <Fragment key={idx}>
-                                    <RadioGroup.Option
-                                        key={option.name}
-                                        value={option}
-                                        className={({ active, checked }) =>
-                                            classNames(
-                                                active ? '' : '',
-                                                checked ? '' : ' ',
-                                                'cursor-pointer flex items-center justify-center rounded-md py-3 px-3 text-sm md:text-base font-extrabold uppercase bg-transparent relative'
-                                            )
-                                        }
-                                    >
-                                        {({ checked }) => (
-                                            <RadioGroup.Label
-                                                as="span"
-                                                className={`${checked && 'text-primary underline decoration-[3px] underline-offset-1 decoration-secondary'} `}
-                                            >
-                                                {option.name}
-                                                <sup>{option.count}</sup>
-                                            </RadioGroup.Label>
-                                        )}
-                                    </RadioGroup.Option>
-                                    {idx < skillFilters.length - 1 && (
-                                        <span className="text-sm md:text-base font-extrabold flex items-center">
-                                            /
-                                        </span>
-                                    )}
-                                </Fragment>
-                            ))}
+                <RadioGroup
+                    value={skillFilter}
+                    onChange={setSkillFilter}
+                    className="mt-2"
+                >
+                    <RadioGroup.Label className="sr-only">
+                        Skill filter
+                    </RadioGroup.Label>
+                    <div className="flex">
+                        <div className="flex items-center font-normal md:font-medium mr-1 ">
+                            Filter:
                         </div>
-                    </RadioGroup>
-
-                    <ul
-                        role="list"
-                        className="mt-3 grid grid-cols-2 gap-5 sm:grid-cols-3 sm:gap-6 lg:grid-cols-4 lg:gap-7"
-                    >
-                        <AnimatePresence>
-                            {filteredSkills.map(skill => (
-                                <motion.li
-                                    key={skill.name}
-                                    className="col-span-1 flex rounded-md shadow-md"
-                                    initial={{ opacity: 0.4, scale: 0.7 }}
-                                    animate={{ opacity: 1, scale: 1 }}
-                                    exit={{ opacity: 0.25, scale: 0.7 }}
-                                    transition={{
-                                        ease: 'easeInOut',
-                                        duration: 0.35,
-                                    }}
+                        {skillFilters.map((option, idx) => (
+                            <Fragment key={idx}>
+                                <RadioGroup.Option
+                                    key={option.name}
+                                    value={option}
+                                    className={({ active, checked }) =>
+                                        classNames(
+                                            active ? '' : '',
+                                            checked ? '' : ' ',
+                                            'cursor-pointer flex items-center justify-center rounded-md py-3 px-3 text-sm md:text-base font-extrabold uppercase bg-transparent relative'
+                                        )
+                                    }
                                 >
-                                    <div
-                                        className={classNames(
-                                            skill.bgColor,
-                                            'bg-base-300 bg-opacity-50 flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white'
-                                        )}
-                                    >
-                                        <div className="flex flex-1 h-5/6 w-auto justify-center">
-                                            {skill.icon}
-                                        </div>
+                                    {({ checked }) => (
+                                        <RadioGroup.Label
+                                            as="span"
+                                            className={`${checked && 'text-primary underline decoration-[3px] underline-offset-1 decoration-secondary'} `}
+                                        >
+                                            {option.name}
+                                            <sup>{option.count}</sup>
+                                        </RadioGroup.Label>
+                                    )}
+                                </RadioGroup.Option>
+                                {idx < skillFilters.length - 1 && (
+                                    <span className="text-sm md:text-base font-extrabold flex items-center">
+                                        /
+                                    </span>
+                                )}
+                            </Fragment>
+                        ))}
+                    </div>
+                </RadioGroup>
+
+                <ul
+                    role="list"
+                    className="mt-3 grid grid-cols-2 gap-5 sm:grid-cols-3 sm:gap-6 lg:grid-cols-4 lg:gap-7"
+                >
+                    <AnimatePresence>
+                        {filteredSkills.map(skill => (
+                            <motion.li
+                                key={skill.name}
+                                className="col-span-1 flex rounded-md shadow-md"
+                                initial={{ opacity: 0.4, scale: 0.7 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0.25, scale: 0.7 }}
+                                transition={{
+                                    ease: 'easeInOut',
+                                    duration: 0.35,
+                                }}
+                            >
+                                <div
+                                    className={classNames(
+                                        skill.bgColor,
+                                        'bg-base-300 bg-opacity-50 flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white'
+                                    )}
+                                >
+                                    <div className="flex flex-1 h-5/6 w-auto justify-center">
+                                        {skill.icon}
                                     </div>
-                                    <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-                                        <div className="flex-1 truncate px-4 py-2 text-sm">
-                                            <p className="font-medium text-gray-900">
-                                                {skill.name}
-                                            </p>
-                                            <p className="text-gray-500">
-                                                {skill.category}
-                                            </p>
-                                        </div>
+                                </div>
+                                <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                                    <div className="flex-1 truncate px-4 py-2 text-sm">
+                                        <p className="font-medium text-gray-900">
+                                            {skill.name}
+                                        </p>
+                                        <p className="text-gray-500">
+                                            {skill.category}
+                                        </p>
                                     </div>
-                                </motion.li>
-                            ))}
-                        </AnimatePresence>
-                    </ul>
-                </div>
-            </Transition>
-        </StackedWrapper>
+                                </div>
+                            </motion.li>
+                        ))}
+                    </AnimatePresence>
+                </ul>
+            </div>
+        </StackedWrapperWithFade>
     );
 };
 
