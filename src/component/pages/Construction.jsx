a4ae8dd34608f0700f51f3eadf4be39ae8f3ba0a
Brian Kim <briandhkim93@gmail.com>
2024-03-08 22:04:17 -0800

    construction cleanup

--- src/component/pages/Construction.jsx
+++ src/component/pages/Construction.jsx
@@ -1,29 +1,41 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import background from '../../assets/images/hexa.JPG';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Construction = () => {
     return (
         <>
-            <main className="grid min-h-screen place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
-                <div className="text-center">
-                    <p className="text-base font-semibold text-green-600">hi</p>
-                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-100 sm:text-5xl">
+            <main className="relative isolate min-h-screen flex items-center">
+                <img
+                    src={background}
+                    alt=""
+                    className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+                />
+                <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8 ">
+                    <p className="text-base font-semibold leading-8 text-white">
+                        hi
+                    </p>
+                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
                         Coming "soon"
                     </h1>
-                    <p className="mt-6 text-base leading-7 text-gray-300">
+                    <p className="mt-4 text-base text-white/70 sm:mt-6">
                         Area under construction
                     </p>
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <div className="mt-10 flex justify-center">
                         <a
-                            target="_blank"
                             href="https://www.linkedin.com/in/bdhk/"
                             rel="noreferrer"
-                            className="rounded-md bg-[#0a66c2] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#004183] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#004183]"
+                            className="text-sm font-semibold leading-7 text-white "
                         >
-                            LinkedIn
+                            Linked
                             <FontAwesomeIcon
-                                className="h-5 w-5 ml-1"
+                                className="ml-[1.5px]"
                                 icon={faLinkedin}
+                            />
+                            <FontAwesomeIcon
+                                className="ml-2"
+                                icon={faArrowUpRightFromSquare}
                             />
                         </a>
                     </div>
